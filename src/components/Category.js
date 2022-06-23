24d3e0cf45b7e4e731d8ae9b510ff18816b1eeb4
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-23 19:21:54 +0300

    Modify reducer function

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <>
-    <button type="button">Check status</button>
-  </>
-);
+const Categories = () => {
+  const checking = useSelector((state) => state.checkStatusReducer);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <h1>{checking}</h1>
+      <button type="button" onClick={() => dispatch(checkStatus())}>Check status</button>
+    </>
+  );
+};
 
 export default Categories;
