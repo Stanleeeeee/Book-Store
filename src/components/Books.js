c2ae3aa32709cc86450f2326f24a34184dbf13ee
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-24 22:24:04 +0100

    Add updated books

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,26 +3,18 @@ import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import { removeBook } from '../redux/books/books';
 
-const Books = (props) => {
-  const {
-    book: {
-      books,
-      author,
-      id,
-      category,
-    },
-
-  } = props;
+const Books = ({ book }) => {
+  const [id, [item]] = book;
   const dispatch = useDispatch();
 
   return (
     <>
       <li key={id} className="books-content">
-        {books}
+        {item.title}
         {' by '}
-        {author}
+        {item.author}
         {' - '}
-        {category}
+        {item.category}
       </li>
       <button type="button" id={id} onClick={() => dispatch(removeBook(id))}>Remove</button>
     </>
@@ -32,7 +24,7 @@ const Books = (props) => {
 Books.propTypes = {
   book: PropTypes.shape({
     id: PropTypes.number.isRequired,
-    books: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
