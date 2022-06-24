1ecdeb97b7a84ec0f72db22519831c3a9defb7fd
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-24 22:22:40 +0100

    Add updated bookcontainer

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { connect, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 import BookContent from './Book';
+import { fetchBook } from '../redux/books/books';
 import AddBook from './AddBook';
 
-const BookContainer = () => {
-  const books = useSelector((state) => state.bookReducer);
+const BookContainer = ({ fetchBooks }) => {
+  const infor = useSelector((state) => state.bookReducer);
+  const books = infor;
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   return (
     <div className="main-container">
       <BookContent books={books} />
@@ -13,4 +20,12 @@ const BookContainer = () => {
   );
 };
 
-export default BookContainer;
+BookContainer.propTypes = {
+  fetchBooks: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchBooks: () => dispatch(fetchBook()),
+});
+
+export default connect(null, mapDispatchToProps)(BookContainer);
