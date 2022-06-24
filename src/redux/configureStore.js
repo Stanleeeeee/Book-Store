3da25f414baf8a0dabbceb832efd1bc070e22d01
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-24 22:13:27 +0100

    Add middleware thrunk to project

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import bookReducer from './books/books';
-import checkStatusReducer from './categories/category';
+import checkStatusReducer from './categories/categories';
 
 const reducer = combineReducers({
   bookReducer,
   checkStatusReducer,
 });
 
-const store = createStore(reducer);
-
+const store = createStore(reducer, applyMiddleware(thunk));
 export default store;
