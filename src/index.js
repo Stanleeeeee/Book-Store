fe49eb73e679bdea0c87d8ab7f961f3285760c45
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-25 17:12:09 +0100

    Add react circular progressbar dependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+// import './components/Navbar.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
