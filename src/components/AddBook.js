d3981afb96e146956c494f2ccd4b960e92b55705
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-25 16:13:26 +0100

    Update addbook.js

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,22 +1,26 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
+import './AddBook.css';
 
 const AddBook = () => {
   const dispatch = useDispatch();
-  const [infor, setInfor] = useState({ book: '', author: '' });
+  const [infor, setInfor] = useState({ book: '', author: '', category: '' });
 
   const inputData = (e) => {
     const inputName = e.target.name;
     switch (inputName) {
       case 'title':
-        setInfor({ book: e.target.value, author: infor.author });
+        setInfor({ book: e.target.value, author: infor.author, category: infor.category });
         break;
       case 'author':
-        setInfor({ book: infor.book, author: e.target.value });
+        setInfor({ book: infor.book, author: e.target.value, category: infor.category });
+        break;
+      case 'category':
+        setInfor({ book: infor.book, author: infor.author, category: e.target.value });
         break;
       default:
-        setInfor({ book: infor.book, author: infor.author });
+        setInfor({ book: infor.book, author: infor.author, category: infor.category });
     }
   };
 
@@ -25,9 +29,10 @@ const AddBook = () => {
       <div className="add-container">
         <h2>ADD NEW BOOK</h2>
         <form action="#" method="post">
-          <input type="text" name="title" placeholder="Book Title" onChange={inputData} required />
-          <input type="text" name="author" placeholder="Book Author" onChange={inputData} required />
-          <button type="button" onClick={() => dispatch(addBook(infor))}>Add Book</button>
+          <input type="text" name="title" className="input" placeholder="Book Title" onChange={inputData} required />
+          <input type="text" name="author" className="input" placeholder="Book Author" onChange={inputData} required />
+          <input type="text" name="category" className="input" placeholder="category" onChange={inputData} required />
+          <button type="button" className="add-btn" onClick={() => dispatch(addBook(infor))}>Add Book</button>
         </form>
 
       </div>
